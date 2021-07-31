92318c2ec34be3e2ded8a5c27bc14052c6a2b4b0
lihua <lihua@163.com>
2021-07-31 18:56:12 +0800

    th commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,20 @@
+package com.atguigu.git;
+
+/**
+ * @author
+ * @create 2021-07-31 18:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello git 2");
+        System.out.println("hello git 3");
+        System.out.println("hello git 4");
+        System.out.println("hello git 4");
+        System.out.println("hello git 4");
+        System.out.println("hello git 4");
+        System.out.println("hello git 4");
+        System.out.println("hello git 4");
+    }
+
+}
