9813fbecc2ac53fdfb620b7073bf9cbddc7048e1
lihua <lihua@163.com>
2021-07-31 17:42:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GetTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author
+ * @create 2021-07-31 17:37
+ */
+public class GetTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
