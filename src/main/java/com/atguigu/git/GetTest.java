84f29fc6f8fe64b79f14d09101b6abe5d76a7c67
lihua <lihua@163.com>
2021-07-31 18:32:27 +0800

    push commit

--- src/main/java/com/atguigu/git/GetTest.java
+++ src/main/java/com/atguigu/git/GetTest.java
@@ -13,6 +13,7 @@ public class GetTest {
         System.out.println("hello git 4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 
 }
