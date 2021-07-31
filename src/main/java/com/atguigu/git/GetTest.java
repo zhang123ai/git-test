eb4485a794c2e0ec9ba8685a09e440bc38528c1c
lihua <lihua@163.com>
2021-07-31 17:45:08 +0800

    third commit

--- src/main/java/com/atguigu/git/GetTest.java
+++ src/main/java/com/atguigu/git/GetTest.java
@@ -9,6 +9,7 @@ public class GetTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello git 2");
+        System.out.println("hello git 3");
     }
 
 }
